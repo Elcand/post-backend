169030365e88fe486571381abe0186e52daf3f0d
Elcand <tjandraraharja99@gmail.com>
2025-07-07 14:00:23 +0700

    feat: api User

--- app/Http/Controllers/USerController.php
+++ app/Http/Controllers/USerController.php
@@ -2,9 +2,59 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
-class USerController extends Controller
+class UserController extends Controller
 {
-    //
+    public function index()
+    {
+        return User::with('address')->get();
+    }
+
+    public function show($id)
+    {
+        return User::with('address')->findOrFail($id);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'username' => 'required|string|unique:users,username',
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users,email',
+            'address.street' => 'required|string',
+            'address.suite' => 'required|string',
+            'address.city' => 'required|string',
+            'address.zipcode' => 'required',
+        ]);
+
+        $user = User::create([
+            'username' => $request->username,
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt('password')
+        ]);
+
+        $user->address()->create($request->address);
+
+        return response()->json($user->load('address'), 201);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user = User::findOrFail($id);
+        $user->update($request->only('username', 'name', 'email'));
+
+        if ($request->has('address')) {
+            $user->address()->update($request->address);
+        }
+
+        return response()->json($user->load('address'));
+    }
+
+    public function destroy($id)
+    {
+        return User::destroy($id);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('api')->group(function () {
     Route::apiResource('posts', PostController::class);
+    Route::apiResource('users', UserController::class);
 });
