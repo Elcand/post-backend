834eb56e482ead1af2416882ad7963aa563b86eb
Elcand <tjandraraharja99@gmail.com>
2025-07-07 10:35:12 +0700

    feat: make api post

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,6 @@
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
-Route::apiResource('posts', PostController::class);
+Route::prefix('api')->group(function () {
+    Route::apiResource('posts', PostController::class);
+});
