1ac43893209a07c945decd232e1a94748aaccdac
Elcand <tjandraraharja99@gmail.com>
2025-07-17 16:19:33 +0700

    set up breeze

--- routes/api.php
+++ routes/api.php
@@ -1,31 +1,8 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\PostController;
-use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
 
-// Tambahkan middleware Sanctum agar SPA bisa pakai cookie
-Route::middleware([
-    EnsureFrontendRequestsAreStateful::class,
-    'web'
-])->group(function () {
-    Route::get('/sanctum/csrf-cookie', function () {
-        return response()->json(['csrf_cookie' => 'set']);
-    });
-});
-
-// Route::post('/users', [UserController::class, 'store']);
-// Route::get('/users', [UserController::class, 'index']);
-
-Route::get('/user/{id}', function (Request $request) {
+Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
-});
-
-Route::prefix('api')->group(function () {
-    Route::apiResource('posts', PostController::class);
-    Route::apiResource('users', UserController::class);
-    Route::get('users/{user}/posts', [PostController::class, 'getUserPosts']);
 });
--- routes/web.php
+++ routes/web.php
@@ -1,33 +1,9 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\PostController;
-use Illuminate\Auth\Events\Verified;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Foundation\Auth\EmailVerificationRequest;
-use Illuminate\Http\Request;
-
-// Route::middleware('web')->group(function () {
-Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::get('/login', [AuthController::class, 'login'])->name('login');
-Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-Route::get('/register', [AuthController::class, 'register'])->name('register');
-
-Route::get('/email/verify', function () {
-    return view('auth.verify-email'); // Buat view ini kalau mau
-})->middleware('auth')->name('verification.notice');
-
-Route::get('/email/verify/{id}/{hash}', function (EmailVerificationRequest $request) {
-    $request->fulfill();
-    return redirect('/dashboard');
-})->middleware(['auth', 'signed'])->name('verification.verify');
-
-Route::post('/email/verification-notification', function (Request $request) {
-    $request->user()->sendEmailVerificationNotification();
-    return back()->with('message', 'Verification link sent!');
-})->middleware(['auth', 'throttle:6,1'])->name('verification.send');
-
 
 Route::get('/', function () {
-    return view('welcome');
+    return ['Laravel' => app()->version()];
 });
+
+require __DIR__.'/auth.php';
