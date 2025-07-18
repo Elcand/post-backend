72b858f5d1c62ac4017c2f91d29d2832dc8fd534
Elcand <tjandraraharja99@gmail.com>
2025-07-18 09:10:01 +0700

    add new router

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('posts', PostController::class);
+    Route::apiResource('users', UserController::class);
+    Route::get('users/{user}/posts', [PostController::class, 'getUserPosts']);
 });
